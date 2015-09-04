a3d353ce7f18ac1e3cfb53554072f519c6a8a232
vladimir.romanov <vladimir.n.romanov@gmail.com>
2015-09-04 17:54:36 +0300

    new examples

--- src/main/resources/primes1.sql
+++ src/main/resources/primes1.sql
@@ -1,10 +1,3 @@
-select r
-from (
-  select rownum+90000 r from dual connect by rownum < 100000
-)
+select r from (select rownum + 90000 r from dual connect by rownum < 100000)
 where
-  r=2
-  or 0 not in(
-    select mod(r,rownum+1) from dual
-    connect by rownum<sqrt(r)
-  )
+  r=2 or 0 not in( select mod( r, rownum+1) from dual connect by rownum < sqrt(r))
